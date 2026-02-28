3d7bd236c14fb05ce6b413bb2ffed6dd8f750e90
Shawn Zeng <szeng26@HQ.AD.HILTON.COM>
2026-02-28 15:59:10 +0800

    fix: remove invalid importmap and set base path for Cloudflare Pages

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, (process as any).cwd(), '');
 
   return {
+    base: '/',
     plugins: [react()],
     define: {
       'process.env.API_KEY': JSON.stringify(env.API_KEY)
